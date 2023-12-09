339337e762563dbaf5f595945a9e3aacaf378188
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-12-09 22:42:10 +0900

    feat(Car): 자동차 이름의 길이가 올바른지 판별하는 기능

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,13 +1,25 @@
-package racingcar.controller;
+package racingcar.model;
 
 public class Car {
     private static String BASH = "-";
+    private static int MAX_NAME_LENGTH = 5;
     private String name;
     private int distance;
 
     public Car(String name) {
+        validCarName(name);
         this.name = name;
         distance = 0;
+    }
+
+    private void validCarName(String name) {
+        if (!checkNameLength(name.trim().length())) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkNameLength(int nameLength) {
+        return nameLength >= 1 && nameLength <= MAX_NAME_LENGTH;
     }
 
     public int getDistance() {

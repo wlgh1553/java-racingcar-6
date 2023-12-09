4f4381df057a37d2b3238e2b2195cb59ac9ab0bd
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-12-09 22:16:23 +0900

    feat(Cars): 우승자를 찾는 기능

--- src/test/java/racingcar/controller/Cars.java
+++ src/test/java/racingcar/controller/Cars.java
@@ -3,7 +3,9 @@ package racingcar.controller;
 import static racingcar.util.Random.getMoveNumber;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
     private List<Car> racingCars;
@@ -17,5 +19,15 @@ public class Cars {
 
     public void update() {
         racingCars.forEach(car -> car.move(getMoveNumber()));
+    }
+
+    private int getWinnerDistance() {
+        Car fastCar = racingCars.stream().max(Comparator.comparing(Car::getDistance)).orElseThrow();
+        return fastCar.getDistance();
+    }
+
+    public List<Car> getWinners() {
+        int winningDistance = getWinnerDistance();
+        return racingCars.stream().filter(car -> car.getDistance() >= winningDistance).collect(Collectors.toList());
     }
 }
